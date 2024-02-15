b61b7c1c5df3f4ae31553746e81e8936b56b55c4
CLiF <clif1593@gmail.com>
2024-02-15 12:29:51 +0900

    Hotfix

--- ADOFAI_DynamicDecoration/File.cpp
+++ ADOFAI_DynamicDecoration/File.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <fstream>
-#include <Windows.h>
 #include <atlstr.h>
 #include <shlobj_core.h>
 #include <filesystem>
 #include "json/json.h"
 #include "File.h"
+
+#include "ProtableFileDialogs.h"
+#define _HAS_STD_BYTE 0
 using namespace std;
 
 void ReadJson(const std::string& file, Json::Value& root) {
-	std::ifstream config_doc(file.c_str(), std::ifstream::binary);
+	std::ifstream config_doc(filesystem::path(filesystem::u8path(file)), std::ifstream::binary);
+	if (!config_doc.is_open()) {
+		cout << endl;
+		cout << "!! Error : Cannot open the file. !!" << endl;
+		cout << "Details : " << "File not found" << endl;
+		cout << "맵을 읽어오는 과정에서 문제가 발생하였습니다. (There was a problem reading the map.)" << endl;
+		cout << "위 오류 위치를 참고하여 맵 파일을 직접 수정해주세요. (Please correct the map file directly by referring to the error location above.)" << endl << endl;
+		system("pause");
+		exit(-1);
+	}
 	//check for UTF-8 BOM and remove if needed
 	char b[4] = { 0, 0, 0, 0 };
 	char bom[] = { (char)239, (char)187, (char)191, 0 };
@@ -44,20 +55,29 @@ void WriteJson(const Json::Value& value, string location) {
 }
 
 string OpenFileName(FileType type) {
+	vector<string> selection;
+	switch (type) {
+	case ADOFAI:
+		selection = pfd::open_file("Open File", "", { "ADOFAI Map File(*.adofai)", "*.adofai" }, pfd::opt::force_path).result();
+		break;
+	case VIDEO:
+		selection = pfd::open_file("Open File", "", { "Video File(*.mp4)", "*.mp4" }, pfd::opt::force_path).result();
+		break;
+	}
+	if (selection.empty()) {
+		return "";
+	}
+	return selection.front();
+}
+
+string OpenFileNameADOFAI() {
 	HDC hdc;
 	PAINTSTRUCT ps;
 	OPENFILENAME OFN;
 	wchar_t lpstrFile[MAX_PATH] = L"";
 	memset(&OFN, 0, sizeof(OPENFILENAME));
 	OFN.lStructSize = sizeof(OPENFILENAME);
-	switch (type) {
-	case ADOFAI:
-		OFN.lpstrFilter = L"ADOFAI Map File(*.adofai)\0*.adofai\0";
-		break;
-	case VIDEO:
-		OFN.lpstrFilter = L"Video File(*.mp4)\0*.mp4\0";
-		break;
-	}
+	OFN.lpstrFilter = L"ADOFAI Map File(*.adofai)\0*.adofai\0";
 	OFN.lpstrFile = lpstrFile;
 	OFN.hwndOwner = NULL;
 	OFN.nMaxFile = 256;
@@ -71,21 +91,4 @@ string OpenFileName(FileType type) {
 	if (GetOpenFileName(&OFN) != 0)
 		str = CW2A(OFN.lpstrFile);
 	return str;
-}
-
-string OpenFolderName() {
-	BROWSEINFO   bi;
-	LPITEMIDLIST  idl;
-	TCHAR szPathname[MAX_PATH] = { 0, };
-	ZeroMemory(&bi, sizeof(BROWSEINFO));
-	bi.hwndOwner = NULL;
-	bi.pszDisplayName = szPathname;
-	bi.ulFlags = BIF_EDITBOX | 0x0040;
-	idl = SHBrowseForFolder(&bi);
-	if (idl) {
-		SHGetPathFromIDList(idl, szPathname);
-	}
-	wstring conv(szPathname);
-	string ret(conv.begin(), conv.end());
-	return ret;
 }
--- ADOFAI_DynamicDecoration/File.h
+++ ADOFAI_DynamicDecoration/File.h
@@ -1,10 +1,8 @@
 #pragma once
 #include "json/json.h"
-using namespace std;
 
 enum FileType { ADOFAI, VIDEO };
 
 void ReadJson(const std::string& file, Json::Value& root);
-void WriteJson(const Json::Value& value, string location);
-string OpenFileName(FileType type);
-string OpenFolderName();
+void WriteJson(const Json::Value& value, std::string location);
+std::string OpenFileName(FileType type);
